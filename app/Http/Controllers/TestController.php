50ac9ef4653a6c60594df377db597c63f57d7c07
satokouichi <tnyrb006@yahoo.co.jp>
2024-06-29 02:03:55 +0900

    Redis

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Redis;
-// use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Cache;
 use App\Models\User;
 
 class TestController extends Controller
