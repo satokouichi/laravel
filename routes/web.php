a7fa8b1d2d9025cffb5ee2afc64efe2e24740440
satokouichi <tnyrb006@yahoo.co.jp>
2024-06-29 14:42:19 +0900

    Laravel Basic認証を非表示

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\BasicAuthMiddleware;
 use App\Http\Controllers\TestController;
 
-Route::middleware([BasicAuthMiddleware::class])->group(function () {
+// Route::middleware([BasicAuthMiddleware::class])->group(function () {
     Route::get('/', [TestController::class, 'index']);
-});
-
-// Route::get('/', function () {
-//     return 'Success';
-// })->middleware('auth.basic');
+// });
